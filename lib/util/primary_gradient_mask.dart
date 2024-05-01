3d954a239641c78ff7db50c0f4777ad360f93d6f
st_bestswlkh0310_for_mac <hhhello0507@gmail.com>
2024-05-01 14:12:37 +0900

    feat : create primary gradient mask

---
+++ lib/util/primary_gradient_mask.dart
@@ -0,0 +1,20 @@
+import 'package:chatping/color/build_context+.dart';
+import 'package:flutter/material.dart';
+
+import '../color/gradient/primary.dart';
+
+class PrimaryGradientMask extends StatelessWidget {
+  const PrimaryGradientMask({super.key, required this.child});
+
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return ShaderMask(
+      blendMode: BlendMode.srcIn,
+      shaderCallback: (bounds) => gradient(Primary.p50)
+          .createShader(Rect.fromLTWH(0, 0, bounds.width, bounds.height)),
+      child: child,
+    );
+  }
+}
